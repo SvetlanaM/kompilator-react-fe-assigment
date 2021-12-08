73522a3ec1bd8b4de37c5679e1301b8bad964199
Ondra Mařík <ondra.marik@gmail.com>
2021-12-08 20:50:50 +0100

    Improve form

--- components/UserForm.tsx
+++ components/UserForm.tsx
@@ -2,11 +2,13 @@
 type UserFormProps = {
     name?: string
     birthdate?: Date
+    about?: string
     height?: number
+    isHuman?: boolean
     drinkPreference?: string
 }
 
-const UserForm = ( { name, birthdate, height, drinkPreference } : UserFormProps) => {
+const UserForm = ( { name, birthdate, about, height, isHuman, drinkPreference } : UserFormProps) => {
 
     const handleSubmit = (data) => {
         console.log(data)
@@ -14,13 +16,23 @@ const UserForm = ( { name, birthdate, height, drinkPreference } : UserFormProps)
 
     return (
         <form onSubmit={handleSubmit}>
-            <label>
-                Name:
-                <input type='text' name='name' value={name} />
-                <input type='datetime-local' name='birthdate' value={birthdate?.toString()} />
-                <input type='number' name='height' value={height} />
-                <select name='drink_preference' value={drinkPreference} />
-            </label>
+            <label>Name:</label>
+            <input type='text' name='name' value={name} />
+            <label>Birthdate:</label>
+            <input type='date' name='birthdate' value={birthdate?.toString()} />
+            <label>About</label>
+            <textarea name='about' value={about} />
+            <label>Height:</label>
+            <input type='number' name='height' checked={isHuman ?? true} />
+            <label>Human:</label>
+            <input type='checkbox' name='is_human' value={height} />
+            <label>Drink preference:</label>
+            <select name='drink_preference' value={drinkPreference}>
+                <option value='whiskey'>Whiskey</option>
+                <option value='beer'>Beer</option>
+                <option value='wine'>Wine</option>
+                <option value='vodka'>Vodka</option>
+            </select>
             <input type='submit' value='Submit' />
         </form>
     )
